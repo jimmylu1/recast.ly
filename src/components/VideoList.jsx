01f779b6131d26677f06a880af31cc455a334d2b
jimmylu1 <jimmy88lu@gmail.com>
2019-04-22 20:13:19 -0700

    Import Success

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,4 +1,4 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -6,6 +6,7 @@ var VideoList = () => (
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
   </div>
+
 );
 
 // PropTypes tell other developers what `props` a component expects
@@ -15,5 +16,17 @@ VideoList.propTypes = {
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
-// `var` declarations will only exist globally where explicitly defined.
+// `var` declarations will only exist globally where explicitly defined. 
+
 export default VideoList;
+
+
+
+
+/* <div className="video-list">
+{props.video.map(video => (
+  <VideoListEntry title = {props.video.snippet.title}
+    description = {props.video.snippet.description} />
+))}
+</div> */
+
